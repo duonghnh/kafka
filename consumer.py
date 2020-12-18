4794e02c68d48a7f1e0b130b2afbee9b0169c5f1
duonghnh <37147024+duonghnh@users.noreply.github.com>
2020-12-18 13:49:36 +0700

    Create consumer.py

---
+++ consumer.py
@@ -0,0 +1,6 @@
+from kafka import KafkaConsumer
+brokers = ['localhost:' str(i) for in in range(9092, 9095)]
+consumer = KafkaConsumer('Topic1',bootstrap_servers=brokers,auto_offset_reset='earliest')
+for message in consumer:
+  print("%s: partition=%d offset=%d value=%s" % (message.topic, message.partition,message.offset,message.value))
+ 
